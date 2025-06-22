49b6d0baa9a810b3c2c15aa8a76cde1f08c8258a
ZhaoLinXing <371132562@qq.com>
2025-06-22 20:21:30 +0800

    feat(frontend): 设置 React Router以管理前端路由

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,12 @@ import 'normalize.css'
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RouterProvider } from 'react-router'
 
-import App from './App.tsx'
+import router from './router.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 )
