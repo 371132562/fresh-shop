1cd6355cfae357fdbf3c19cc6cf3adea6445e9c8
ZhaoLinXing <371132562@qq.com>
2025-06-24 20:49:34 +0800

    refactor(frontend): 优化页面布局和国际化支持
    - 在 Layout 组件中添加 z-index 属性以修复头部层级问题
    -增加 ConfigProvider 组件以支持国际化
    - 调整页面主体的样式以改善布局- 更新 package.json 和 pnpm-lock.yaml 文件

--- frontend/src/components/Layout/index.tsx
+++ frontend/src/components/Layout/index.tsx
@@ -7,12 +7,12 @@ export const Component: FC = () => {
 
   return (
     <div className="ml-auto mr-auto h-dvh w-dvw max-w-3xl bg-gray-100">
-      <header className="h-13 sticky top-0 flex flex-row bg-white">
+      <header className="h-13 z-1000 sticky top-0 flex flex-row bg-white">
         <nav className="m-2 ml-1 flex h-10 w-10 cursor-pointer items-center justify-center rounded-full transition duration-200 active:bg-gray-200">
           <UnorderedListOutlined className="text-2xl" />
         </nav>
       </header>
-      <div className="flex w-full flex-col items-center">{outlet}</div>
+      <div className="flex w-full flex-col items-center px-3 py-2">{outlet}</div>
     </div>
   )
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,22 @@
 import './index.css'
 import 'normalize.css'
+import 'dayjs/locale/zh-cn'
 
+import { ConfigProvider } from 'antd'
+import zhCN from 'antd/locale/zh_CN'
+import dayjs from 'dayjs'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router'
 
 import router from './router.tsx'
 
+dayjs.locale('zh-cn')
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <ConfigProvider locale={zhCN}>
+      <RouterProvider router={router} />
+    </ConfigProvider>
   </StrictMode>
 )
